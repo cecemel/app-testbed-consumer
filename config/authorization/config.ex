fcbbacb45b4ec57709a95fb478734220ec5550d6
felix <ruiz.felixruiz@gmail.com>
2021-10-21 07:55:48 +0200

    prepare consumption of files

---
+++ config/authorization/config.ex
@@ -0,0 +1,32 @@
+alias Acl.Accessibility.Always, as: AlwaysAccessible
+alias Acl.GraphSpec.Constraint.Resource, as: ResourceConstraint
+alias Acl.GraphSpec, as: GraphSpec
+alias Acl.GroupSpec, as: GroupSpec
+alias Acl.GroupSpec.GraphCleanup, as: GraphCleanup
+
+defmodule Acl.UserGroups.Config do
+  def user_groups do
+    [
+      # Synced files
+      %GroupSpec{
+        name: "synced-files",
+        useage: [:write, :read_for_write, :read],
+        access: %AlwaysAccessible{},
+        graphs: [ %GraphSpec{
+                    graph: "http://mu.semte.ch/graphs/synced-files",
+                    constraint: %ResourceConstraint{
+                      resource_types: [
+                        "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject",
+                        "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#DataContainer"
+                      ]
+                    } } ] },
+      # CLEANUP
+      #
+      %GraphCleanup{
+        originating_graph: "http://mu.semte.ch/application",
+        useage: [:write],
+        name: "clean"
+      }
+    ]
+  end
+end
